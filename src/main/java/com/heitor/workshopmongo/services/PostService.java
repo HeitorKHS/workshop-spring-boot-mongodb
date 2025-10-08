ecf627e996757766cd096b87e512a1d014fd3d2c
unknown <heitorkhs@gmail.com>
2025-10-08 16:59:17 -0300

    Obtendo um post por id

---
+++ src/main/java/com/heitor/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.heitor.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.heitor.workshopmongo.domain.Post;
+import com.heitor.workshopmongo.repository.PostRepository;
+import com.heitor.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
